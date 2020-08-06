99009fc8bda39ca6042a3b28ff0c8c2be3bf445a
Aidan Paetsch <aapaetsch@gmail.com>
2020-08-05 21:23:36 -0600

    Updates

---
+++ precache-manifest.efa22a762efae2100ff5d2b2aeee2e74.js
@@ -0,0 +1,50 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "50453eea5875fdc36afd10129de61755",
+    "url": "/Four-In-A-Row-MinMax/index.html"
+  },
+  {
+    "revision": "3859ca0d6e4a7f6e5c5c",
+    "url": "/Four-In-A-Row-MinMax/static/css/2.fc5a9450.chunk.css"
+  },
+  {
+    "revision": "13d38f0908a84dc752e6",
+    "url": "/Four-In-A-Row-MinMax/static/css/main.e13f2c73.chunk.css"
+  },
+  {
+    "revision": "3859ca0d6e4a7f6e5c5c",
+    "url": "/Four-In-A-Row-MinMax/static/js/2.c5f75f95.chunk.js"
+  },
+  {
+    "revision": "3453b8997016469371284a28c0e873e2",
+    "url": "/Four-In-A-Row-MinMax/static/js/2.c5f75f95.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "13d38f0908a84dc752e6",
+    "url": "/Four-In-A-Row-MinMax/static/js/main.6a48a391.chunk.js"
+  },
+  {
+    "revision": "7f8506b4e6c943a8f5bb",
+    "url": "/Four-In-A-Row-MinMax/static/js/runtime-main.b87bfb9d.js"
+  },
+  {
+    "revision": "bd0f8dcea5b6c00033b17ba29284c8d4",
+    "url": "/Four-In-A-Row-MinMax/static/media/boardSquareEmpty.bd0f8dce.jpeg"
+  },
+  {
+    "revision": "da3dbf123674839fcd15a3d9325c588e",
+    "url": "/Four-In-A-Row-MinMax/static/media/boardSquareP1.da3dbf12.jpeg"
+  },
+  {
+    "revision": "6129a79edb6baf4f968a7aa2b9b0f079",
+    "url": "/Four-In-A-Row-MinMax/static/media/boardSquareP2.6129a79e.jpeg"
+  },
+  {
+    "revision": "5d5d9eefa31e5e13a6610d9fa7a283bb",
+    "url": "/Four-In-A-Row-MinMax/static/media/logo.5d5d9eef.svg"
+  },
+  {
+    "revision": "4cd8b8e1103fc9c21b97844358d8a27e",
+    "url": "/Four-In-A-Row-MinMax/static/media/p2Token.4cd8b8e1.jpeg"
+  }
+]);
